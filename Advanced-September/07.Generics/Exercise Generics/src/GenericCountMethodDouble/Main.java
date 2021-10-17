c7c97b29a1e4d2e83d7705ef92ef2461cbf9df71
Silviyaa <silviq.stefanova.sit6b@gmail.com>
2021-10-17 11:44:24 +0300

    07.Generics

---
+++ Advanced-September/07.Generics/Exercise Generics/src/GenericCountMethodDouble/Main.java
@@ -0,0 +1,18 @@
+package GenericCountMethodDouble;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = Integer.parseInt(scanner.nextLine());
+        Box<Double> box = new Box<>();
+        for (int i = 0; i < n; i++) {
+            double value = Double.parseDouble(scanner.nextLine());
+            box.add(value);
+        }
+        double stringToCompare = Double.parseDouble(scanner.nextLine());
+        int greaterElement = box.countOfGreaterItems(stringToCompare);
+        System.out.println(greaterElement);
+    }
+}
